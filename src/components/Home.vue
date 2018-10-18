3a020308e946ea623c64d8d3a56ef7ef54849de8
ashwallsinfinity <ashley.walls@infinityworks.com>
2018-10-18 16:58:03 +0100

    add snail number and trainer to table. Also added predictions banner

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="home">
         <h1 v-if="!loggedIn"  style="color: whitesmoke">Welcome, please register or login.</h1>
+        <div v-if="loggedIn" id="predictions-banner"></div>
         <div v-if="loggedIn" id="predictions"></div>
     </div>
 </template>
@@ -19,17 +20,18 @@
                 this.$store.dispatch('getPredictions')
                     .then((response) => {
                         if(response.data.message !== "Error. No predictions made") {
-                            var printed_table = '<table><tr><th>Race ID</th><th>Snail Name</th> </tr>';
-
+                            var printed_table = '<table><tr><th>Race No.</th><th>Snail No.</th><th>Snail Name</th><th>Trainer</th> </tr>';
+                            
 
                             for (var y = 0; y < response.data.length; y++) {
-                                printed_table += '<tr><td>' + (y + 1) + '</td><td>' + response.data[y][1] + '</td></tr>';
+                                printed_table += '<tr><td>' + (y + 1) + '</td><td>' + response.data[y][1] + '</td><td>' + response.data[y][2] + '</td><td>' + response.data[y][3] + '</td></tr>';
                             }
                             printed_table += '</table>';
                         } else {
                             printed_table = "<center><h3 style='background-color:white; padding:5px;'>You have not made any predictions. To do so <a href='snailx.racing'>Click Here</a></h3></center>"
 
                         }
+                        document.getElementById('predictions-banner').innerHTML = "Your predictions for round " + response.data[0][4] + ":"
                         document.getElementById('predictions').innerHTML = printed_table;
                     })
             }
@@ -51,6 +53,17 @@
         left: 32%;
         width: 50%;
     }
+    /*--- prediction banner styling ---*/
+    #predictions-banner{
+        background-color: white;
+        border: 1px solid black;
+        width: 35%;
+        margin-bottom: 5%;
+        margin-left: 17.5%;
+        text-align: center;
+    }
+    /*--- prediction banner styling end ---*/
+
     /*--- User predictions table styling ---*/
     table {
         width:70%;
