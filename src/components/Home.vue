cc4e0a50fdc8a05eb3e9db162088e617b4c9dd21
JamesSheard <james.sheard@infinityworks.com>
2018-10-18 10:01:41 +0100

    Showing predictions on main page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,7 @@
 <template>
     <div id="home">
         <h1 v-if="!loggedIn"  style="color: whitesmoke">Welcome, please register or login.</h1>
+        <div v-if="loggedIn" id="predictions"></div>
     </div>
 </template>
 
@@ -12,8 +13,40 @@
             ...mapGetters([
                 'loggedIn'
             ])
+        },
+        methods: {
+            getPredictions() {
+                this.$store.dispatch('getPredictions')
+                    .then((response) => {
+                        console.log(response.data.message)
+                        console.log(response.data.length)
+
+                        if(response.data.message !== "Error. No predictions made") {
+
+
+                            var printed_table = '<table><tr><th> Race ID</th><th>Snail Name</th> </tr>';
+
+                            for (var y = 0; y < response.data.length; y++) {
+                                printed_table += '<tr><td>' + (y + 1) + '</td><td>' + response.data[y][1] + '</td></tr>';
+                            }
+                            printed_table += '</table>'-
+                            console.log(printed_table)
+                        } else {
+                            printed_table = "<center><h3 style='background-color:white; padding:5px;'>You have not made any predictions. To do so <a href='snailx.racing'>Click Here</a></h3></center>"
+
+                        }
+                        document.getElementById('predictions').innerHTML = printed_table;
+                    })
+            }
+        },
+        beforeMount() {
+            this.getPredictions()
         }
     }
+
+
+
+
 </script>
 
 <style>
@@ -22,5 +55,18 @@
         top: 40%;
         left: 32%;
         width: 50%;
+    }
+
+    table {
+        width:70%;
+        background-color:white;
+    }
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 15px;
+        text-align: left;
     }
 </style>
