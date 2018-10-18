8126218de6e300952695fdf1a163fe2387bf53c1
JamesSheard <james.sheard@infinityworks.com>
2018-10-18 14:20:31 +0100

    Hot fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -18,19 +18,15 @@
             getPredictions() {
                 this.$store.dispatch('getPredictions')
                     .then((response) => {
-                        console.log(response.data.message)
-                        console.log(response.data.length)
-
                         if(response.data.message !== "Error. No predictions made") {
 
 
-                            var printed_table = '<table><tr><th> Race ID</th><th>Snail Name</th> </tr>';
+                            var printed_table = '<table><tr><th>Race ID</th><th>Snail Name</th> </tr>';
 
                             for (var y = 0; y < response.data.length; y++) {
                                 printed_table += '<tr><td>' + (y + 1) + '</td><td>' + response.data[y][1] + '</td></tr>';
                             }
-                            printed_table += '</table>'-
-                            console.log(printed_table)
+                            printed_table += '</table>';
                         } else {
                             printed_table = "<center><h3 style='background-color:white; padding:5px;'>You have not made any predictions. To do so <a href='snailx.racing'>Click Here</a></h3></center>"
 
