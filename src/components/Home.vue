8b989a64f5e21446db7b85a16d2ee2d825d25982
Emily <emily.birch@infinityworks.com>
2018-10-19 17:50:32 +0100

    basic timer adding stuff

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,151 @@
 <template>
-    <div id="home">
-        <h1 v-if="!loggedIn"  style="color: whitesmoke">Welcome, please register or login.</h1>
-        <div v-if="loggedIn" id="predictions-banner"></div>
-        <div v-if="loggedIn" id="predictions"></div>
-    </div>
+        <div id="home">
+            <div id="home-feature">
+                <h1 v-if="!loggedIn"  style="color: whitesmoke">Welcome - please register or login.</h1>
+                <div v-if="loggedIn" id="predictions-banner"></div>
+                <div v-if="loggedIn" id="predictions"></div>
+            </div>
+
+            <div id="timer-feature">
+                <h1>"Events Countdown Timer"</h1>
+                <div id="timer" class="timer">
+                <!--  Timer Component  -->
+                    <Timer
+                         starttime="Sep 5, 2018 15:37:25"
+                         endtime="Sep 8, 2018 16:37:25"
+                         trans='{
+                         "day":"Day",
+                         "hours":"Hours",
+                         "minutes":"Minuts",
+                         "seconds":"Seconds",
+                         "expired":"Event has been expired.",
+                         "running":"Till the end of event.",
+                         "upcoming":"Till start of event.",
+                         "status": {
+                            "expired":"Expired",
+                            "running":"Running",
+                            "upcoming":"Future"
+                           }}'
+                         >
+
+                    </Timer>
+                    <!--  End! Timer Component  -->
+                </div>
+            </div>
+        </div>
 </template>
+
+
+Vue.component('Timer',{
+	template: `
+  	<div>
+      <div class="day">
+        <span class="number">{{ days }}</span>
+        <div class="format">{{ wordString.day }}</div>
+      </div>
+      <div class="hour">
+        <span class="number">{{ hours }}</span>
+        <div class="format">{{ wordString.hours }}</div>
+      </div>
+      <div class="min">
+        <span class="number">{{ minutes }}</span>
+        <div class="format">{{ wordString.minutes }}</div>
+      </div>
+      <div class="sec">
+        <span class="number">{{ seconds }}</span>
+        <div class="format">{{ wordString.seconds }}</div>
+      </div>
+      <div class="message">{{ message }}</div>
+      <div class="status-tag" :class="statusType">{{ statusText }}</div>
+    </div>
+  `,
+  props: ['starttime','endtime','trans'] ,
+  data: function(){
+  	return{
+    	timer:"",
+      wordString: {},
+      start: "",
+      end: "",
+      interval: "",
+      days:"",
+      minutes:"",
+      hours:"",
+      seconds:"",
+      message:"",
+      statusType:"",
+      statusText: "",
+
+    };
+  },
+  created: function () {
+        this.wordString = JSON.parse(this.trans);
+    },
+  mounted(){
+    this.start = new Date(this.starttime).getTime();
+    this.end = new Date(this.endtime).getTime();
+    // Update the count down every 1 second
+    this.timerCount(this.start,this.end);
+    this.interval = setInterval(() => {
+        this.timerCount(this.start,this.end);
+    }, 1000);
+  },
+  methods: {
+    timerCount: function(start,end){
+        // Get todays date and time
+        var now = new Date().getTime();
+
+        // Find the distance between now an the count down date
+        var distance = start - now;
+        var passTime =  end - now;
+
+        if(distance < 0 && passTime < 0){
+            this.message = this.wordString.expired;
+            this.statusType = "expired";
+            this.statusText = this.wordString.status.expired;
+            clearInterval(this.interval);
+            return;
+
+        }else if(distance < 0 && passTime > 0){
+            this.calcTime(passTime);
+            this.message = this.wordString.running;
+            this.statusType = "running";
+            this.statusText = this.wordString.status.running;
+
+        } else if( distance > 0 && passTime > 0 ){
+            this.calcTime(distance);
+            this.message = this.wordString.upcoming;
+            this.statusType = "upcoming";
+            this.statusText = this.wordString.status.upcoming;
+        }
+    },
+    calcTime: function(dist){
+      // Time calculations for days, hours, minutes and seconds
+        this.days = Math.floor(dist / (1000 * 60 * 60 * 24));
+        this.hours = Math.floor((dist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        this.minutes = Math.floor((dist % (1000 * 60 * 60)) / (1000 * 60));
+        this.seconds = Math.floor((dist % (1000 * 60)) / 1000);
+    }
+
+  }
+});
+
+new Vue({
+  el: "#timer",
+});
+
+
 
 <script>
     import { mapGetters } from "vuex"
     export default {
         name: 'home',
+
         computed: {
             ...mapGetters([
                 'loggedIn'
             ])
         },
+
         methods: {
             getPredictions() {
                 this.$store.dispatch('getPredictions')
@@ -42,6 +173,7 @@
                     })
             }
         },
+
         beforeMount() {
             this.getPredictions()
         }
